ca157830058a735f7025fa29e9f78199f1b58381
abhishek36 <akamble9913@gmail.com>
2021-04-13 08:40:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import react , {useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const emojiCollection = {
+    "😊": "Smiling Emoji",
+    "😂": "Laughining Emoji",
+    "😍": "Loving Emoji",
+    "😎": "Cool Emoji",
+    "😔": "Sad Emoji",
+    "😭": "Crying Emoji",
+    "😮": "Shocked Emoji"
+  }
+  const emojis = Object.keys(emojiCollection);
+
+  const [emoji , setEmoji] = useState("")
+  const [meaning , setMeaning] = useState("Result")
+
+  function emojiClickHandler(e) {
+    setMeaning(emojiCollection[e]);
+  }
+
+  
+  // function handleChange(e) {
+  //   let inputEmoji = e.target.value;
+  //   setEmoji(inputEmoji);
+
+  //   if (emojiCollection[inputEmoji]) {
+  //     setMeaning(emojiCollection[inputEmoji]);
+  //   } else {
+  //     setMeaning("Oops..! Emoji is Not present in Database");
+  //   }
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* <input
+        value={emoji}
+        onChange={handleChange}
+        placeholder="Insert an Emoji to Know Its Meaning"
+        style={{
+          fontSize: "20px",
+          height: "30px",
+          width: "80vw",
+          textAlign: "center",
+          marginTop:"1em",
+        }}
+      /> */}
+       <h1>{meaning}</h1>
+       {emojis.map((e) => (
+        <span
+          key={e}
+          onClick={() => emojiClickHandler(e)}
+          style={{ fontSize: "30px", margin: "5px", cursor: "pointer" }}
         >
-          Learn React
-        </a>
-      </header>
+          {e}
+        </span>
+          ))}
     </div>
   );
 }
